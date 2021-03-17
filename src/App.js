7d1c0a853b6e29a843cc10f6a11de86dc67011fc
Brendan <bstrom@Brendans-MacBook-Pro.local>
2021-03-16 20:37:07 -0700

    error reproduction

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import React from "react";
-import styled from "styled-components";
-// import { faRegular } from "styled-icons";
+import styled, { css } from "styled-components";
+import * as supportedIcons from "./supportedIcons";
 import "./App.css";
 
-// const MySvg = styled(faRegular.HandRock)``;
+const iconCSS = css`
+  position: relative;
+  display: inline-block;
+  width: 1em;
+  height: 1em;
+  user-select: none;
+`;
+
+const styleIcon = (icon) => styled(icon)`
+  ${iconCSS};
+`;
+
+const GenerateStyledIcons = (styledIcons, name) => {
+  styledIcons[name] = styleIcon(supportedIcons[name]);
+  return styledIcons;
+};
+
+const StyledSupportedIcons = Object.keys(supportedIcons).reduce(
+  GenerateStyledIcons,
+  {}
+);
+
+export const getIcon = (name) => StyledSupportedIcons[name];
+
+const MySvg = styled(getIcon("Login"));
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <MySvg /> */}
+        <MySvg />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/supportedIcons.js
@@ -0,0 +1,68 @@
+export { Adjust } from "styled-icons/fa-solid";
+export { Alert } from "styled-icons/remix-fill";
+export { ArrowLeft2 as ArrowLeft } from "styled-icons/icomoon";
+export { Building } from "styled-icons/fa-solid";
+export { Calendar } from "styled-icons/fa-solid";
+export { CaretDown } from "styled-icons/fa-solid";
+export { ChevronRight } from "styled-icons/fa-solid";
+export { Close } from "styled-icons/evil";
+export { Copy } from "styled-icons/fa-solid";
+export { Cursor } from "styled-icons/remix-fill";
+export { CursorText } from "styled-icons/bootstrap";
+export { DotCircle } from "styled-icons/fa-solid";
+export { Edit } from "styled-icons/fa-solid";
+export { EllipsisH } from "styled-icons/fa-solid";
+export { Exclamation } from "styled-icons/fa-solid";
+export { ExclamationCircle } from "styled-icons/fa-solid";
+export { Expand } from "styled-icons/boxicons-regular";
+export { File } from "styled-icons/fa-solid";
+export { Filter } from "styled-icons/fa-solid";
+export { Flag } from "styled-icons/fa-solid";
+export { Globe } from "styled-icons/fa-solid";
+export { Hashtag } from "styled-icons/fa-solid";
+export { IdBadge } from "styled-icons/fa-solid";
+export { Image } from "styled-icons/fa-solid";
+export { InfoCircle } from "styled-icons/fa-solid";
+export { Keyboard } from "styled-icons/fa-solid";
+export { Lock } from "styled-icons/fa-solid";
+export { MinusCircleOutline } from "styled-icons/evaicons-outline";
+export { Palette } from "styled-icons/material";
+export { PaperPlane } from "styled-icons/fa-solid";
+export { Pencil } from "styled-icons/boxicons-solid";
+export { Person } from "styled-icons/material";
+export { Phone } from "styled-icons/fa-solid";
+export { PhotoCamera } from "styled-icons/material-rounded";
+export { Plus } from "styled-icons/fa-solid";
+export { PlusCircle } from "styled-icons/fa-solid";
+export { PlusCircleOutline } from "styled-icons/evaicons-outline";
+export { Save } from "styled-icons/fa-solid";
+export { Search } from "styled-icons/fa-solid";
+export { Settings } from "styled-icons/material-rounded";
+export { Th } from "styled-icons/fa-solid";
+export { Times } from "styled-icons/fa-solid";
+export { TimesCircle } from "styled-icons/fa-solid";
+export { Tint } from "styled-icons/fa-solid";
+export { Trash } from "styled-icons/fa-solid/";
+export { User } from "styled-icons/fa-solid";
+export { Users } from "styled-icons/fa-solid";
+export { WindowClose } from "styled-icons/fa-solid";
+
+// Icon imports from heroicons are intended for use with custom actions only
+export {
+  Archive,
+  ArrowCircleRight,
+  Ban,
+  Bell,
+  Bookmark,
+  ChevronDoubleRight,
+  Exclamation as HeroIconsExclamation,
+  Speakerphone,
+  Flag as HeroIconsFlag,
+  PlusCircle as HeroIconsPlusCircle,
+  XCircle,
+  Users as HeroIconsUsers,
+  UserCircle,
+  UserAdd,
+  UserRemove,
+  Login,
+} from "styled-icons/heroicons-solid";
